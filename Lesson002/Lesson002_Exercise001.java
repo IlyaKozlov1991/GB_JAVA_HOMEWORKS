0df2d529833fec0fd34c84fc165b47bbbbb88376
Ilya Kozlov <ilyakozlovmk91@gmail.com>
2022-12-19 15:21:34 +0300

    added files for Lesson002_Exercise002_19/19/22

--- Lesson002/Lesson002_Exercise001.java
+++ Lesson002/Lesson002_Exercise001.java
@@ -7,14 +7,15 @@ import java.io.IOException;
 public class Lesson002_Exercise001 {
     public static void main(String[] args) throws IOException {
         BufferedReader in_string = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter your string");
+        System.out.println("Enter your string:");
         String s = in_string.readLine();
         String s_reverse = "";
         for (int i = 1; i <= s.length(); i++) { // Reverse the input string
+            // Take each item of input string starting from the end and add to new string
             char c1 = s.charAt(s.length() - i);
             s_reverse = s_reverse + c1;
         }
-        System.out.println("Reversed string: " + s_reverse);
+        // System.out.println("Reversed string: " + s_reverse);
         boolean b = (s_reverse.equals(s)); // Compare input and reversed strings
         if (b == true){
             System.out.println("Yes, string '" +  s + "' is palindrome");
---
+++ Lesson002/Lesson002_Exercise002.java
@@ -0,0 +1,9 @@
+package Lesson002;
+
+import java.io.File;
+
+public class Lesson002_Exercise002 {
+    public static void main(String[] args) {
+        File directory = new File(null);
+    }
+}
